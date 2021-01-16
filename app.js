0b02b522757cf36dc1c47b67f6470f5c8b5e8b3b
Svirriill <sverelb@bk.ru>
2021-01-16 19:18:33 +0300

    level-1

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ const app = express();
 //   credentials: true,
 // };
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
